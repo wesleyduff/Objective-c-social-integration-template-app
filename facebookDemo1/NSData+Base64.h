3d34274e46002e5a5ea5b6dc63818fd2e504f705
Wes Duff <slysop@gmail.com>
2011-03-18 12:01:00 -0500

    cleaning

--- facebookDemo1/NSData+Base64.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-char *NewBase64Encode(const void *inputBuffer, size_t length, bool separateLines, size_t *outputLength);
-
-@interface NSData (Base64)
-- (NSString *)base64EncodedString;
-@end
