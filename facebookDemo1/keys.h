1957a51e7f96d30d6ed29a252358236624fde7ff
Wes Duff <slysop@gmail.com>
2011-03-21 16:00:15 -0500

    ready for git hub

--- facebookDemo1/keys.h
+++ facebookDemo1/keys.h
@@ -10,22 +10,22 @@
 /**
  * FACEBOOK API KEYS AND SECRETS
 */
-#define FacebookAppID @"149013028496185"
-#define FacebookAPIKey @"cc962699775b1070a99e2d533b68ed59"
-#define FacebookAppSecret @"cea276f908007642518fee461457b3be"
-#define FacebookContactEmail @"slysop@gmail.com"
-#define FacebookSupportEmail @"slysop@gmail.com"
+#define FacebookAppID @"<add yours>"
+#define FacebookAPIKey @"<add yours>"
+#define FacebookAppSecret @"<add yours>"
+#define FacebookContactEmail @"<add yours>"
+#define FacebookSupportEmail @"<add yours>"
 
 /**
  * Twitter API KEYS AND SECRETS
  */
-#define twitterConsumerkey @"KvtyfXXknfQioxnuq8nWg"
-#define twitterConsumerSecret @"rMVX4emJI3cnVABH69HwJ1aR6WtdvzMhBNtx4WePzVE"
+#define twitterConsumerkey @"<add yours>"
+#define twitterConsumerSecret @"<add yours>"
 #define twitterRequestTokenURL @"http://twitter.com/oauth/request_token"
 #define twitterAccessTokenURL @"http://twitter.com/oauth/access_token"
 #define twitterAuthorizeURL @"http://twitter.com/oauth/authorize"
-#define twitterAppProviderName @"wesduff.com"
-#define twitterAppPrefix @"socialnetwork-integration"
+#define twitterAppProviderName @"<add yours>"
+#define twitterAppPrefix @"<add yours>"
 /*
  We support hmac-sha1 signatures. We do not support the plaintext signature method. 
  */
@@ -40,15 +40,15 @@
 // Credentials for authentication using OAuth
 // Replace with your own credentials, available at http://api.gowalla.com/api/keys
 
-#define kGowallaAPIKey			@"de5c34980c0e468bb237341c9eab3fd1"
-#define kGowallaAPISecret		@"de73d3b5261944299b71e9c1641c3183"
+#define kGowallaAPIKey			@"<add yours>"
+#define kGowallaAPISecret		@"<add yours>"
 
 // In order to intercept and respond to the OAuth callback, we need to register
 // a custom URL type for the application. This should be unique, to avoid any
 // naming collisions with other applications.
 //
 // Replace this in Info.plist with the callback for your application,
-#define kGowallaRedirectURI		@"wessocialdev://"
+#define kGowallaRedirectURI		@"<add yours>"
 
 // Keys for storing OAuth tokens using NSUserDefaults
 #define kGowallaBasicOAuthAccessTokenPreferenceKey		@"gowalla_basic_oauth_access_token"
